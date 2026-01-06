805fdc56bb192e36cf91d39cc19ff827fcb1f03f
Adriel <adriel_seah@hotmail.com>
2026-01-06 12:40:13 +0800

    Add banner component with disclaimer message

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router-dom';
 import { Sidebar } from './Sidebar';
 import { Masthead } from './Masthead';
 import { Footer } from './Footer';
+import { Banner } from '@/components/ui/Banner';
 
 export function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Masthead />
+      <Banner />
       <div className="flex flex-1 bg-gray-50">
         <Sidebar />
         <main className="flex-1 overflow-auto">
